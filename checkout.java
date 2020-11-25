0be9d159da5502b3b21dea84372e6e159d022715
cocodadog <69607811+cocodadog@users.noreply.github.com>
2020-11-24 20:40:56 -0500

    made the java file
    made the java file

---
+++ checkout.java
@@ -0,0 +1,26 @@
+//checkout project
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+class checkout
+{
+   public static void main (String[] args)
+   {
+      //home page
+      
+      
+      
+      
+      
+      
+      //input list
+      
+      
+      
+      
+      
+      
+      //output list
+      
+      
+   }
+}
