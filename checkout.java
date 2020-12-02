6896fb40cd7f59c52583d3e784ae55694b83d483
squintyeyesmcgee <69607923+squintyeyesmcgee@users.noreply.github.com>
2020-12-02 16:33:50 -0500

    i made a little bit of the welcome page

--- checkout.java
+++ checkout.java
@@ -6,12 +6,25 @@ class checkout
    public static void main (String[] args)
    {
       //home page
+      //call keyboard
+      Scanner keyboard = new Scanner(System.in);
+      //variable for if statement
+      String shopping;
+      shopping = JOptionPane.showInputDialog("Are you sure you want to shop?");
       
-      
-      
-      
-      
-      
+      //if statement
+      if (shopping.equals("yes"))
+      {
+         JOptionPane.showMessageDialog(null ,"Thank you for choosing Walmart");
+      }
+      else if (shopping.equals("no"))
+      {
+         JOptionPane.showMessageDialog(null ,"Goodbye");
+      }
+      else
+      {
+         JOptionPane.showMessageDialog(null ,"Please answer with yes or no");      
+      }     
       //input list
       
       
