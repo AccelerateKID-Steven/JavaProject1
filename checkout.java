ef42f5a20b727b26ba57a40f644dff43f4ace400
BlastedGlass07 <69607761+BlastedGlass07@users.noreply.github.com>
2020-12-26 12:50:27 -0500

    Did taxes and also cleaned up the code a bit
    12/26/2020 Jayden Kim

--- checkout.java
+++ checkout.java
@@ -6,8 +6,10 @@ class checkout
 {
    public static void main (String[] args)
    {
+      
       //call keyboard
       Scanner keyboard = new Scanner(System.in);
+      
       //variable for if statement
       String shopping;
       shopping = JOptionPane.showInputDialog("Are you sure you want to shop?");
@@ -17,10 +19,12 @@ class checkout
       {
          JOptionPane.showMessageDialog(null ,"Thank you for choosing Walmart");
       }
+      
       else if (shopping.equals("no"))
       {
          JOptionPane.showMessageDialog(null ,"Goodbye");
       }
+      
       else
       {
          JOptionPane.showMessageDialog(null ,"Please answer with yes or no");      
@@ -36,49 +40,65 @@ class checkout
       ArrayList<String> shoppingList = new ArrayList<String>( );
       float x, price;
       
-      do{
-      //user input
-      Systeminput = JOptionPane.showInputDialog(null, "type the name of the item");
-      //ifs
-      if(Systeminput.equals("done"))
+      do
       {
-      x = 2;
-      }
-      else
-      {
-         shoppingList.add(Systeminput);
-         System.out.println(shoppingList);
-         x = 1;
-         Systeminput = JOptionPane.showInputDialog(null, "type the price of the item");
+      
+         //user input
+         Systeminput = JOptionPane.showInputDialog(null, "type the name of the item");
+      
+         //ifs
          if(Systeminput.equals("done"))
          {
-            x = 2;
+         x = 2;
          }
          else
          {
-            PriceList.add(Systeminput);
-            System.out.println(PriceList);  
+            shoppingList.add(Systeminput);
+            System.out.println(shoppingList);
+            x = 1;
+            Systeminput = JOptionPane.showInputDialog(null, "type the price of the item");
+         
+            if(Systeminput.equals("done"))
+            {
+               x = 2;
+            }
+         
+            else
+            {
+               PriceList.add(Systeminput);
+               System.out.println(PriceList);  
+            }
          }
-         }
+     
+      }
+     
+     
+      //validaton
+      while(x < 2);
+      for(int z = 0; z < shoppingList.size(); z++)
+      {
+         JOptionPane.showMessageDialog(null,shoppingList.get(z) + " Price: " + PriceList.get(z));
+      }
+     
+      int sum = 0;
+      for(int i=0; i < PriceList.size(); i++)
+      {
+         sum = sum + Integer.parseInt(PriceList.get(i));
+      }
 
-      
-     //validaton
-     }while(x < 2);
-     for(int z = 0; z < shoppingList.size(); z++)
-     {
-     JOptionPane.showMessageDialog(null,shoppingList.get(z) + " Price: " + PriceList.get(z));
-     }
-     int sum = 0;
-     for(int i=0; i < PriceList.size(); i++){
-     sum = sum + Integer.parseInt(PriceList.get(i));
-     }
-
-     JOptionPane.showMessageDialog(null, sum + "$ is your total price");
+      JOptionPane.showMessageDialog(null, sum + "$ is your total price");
       
       
       //output list
       
+      //calculate and show taxes
+      if(sum > 0)
+      {
+         JOptionPane.showMessageDialog(null, (sum * 0.06) + sum + "$ is your total price (including tax)");
+         double totalPrice = sum * 0.06 + sum;  //totalPrice is total price with tax of 6%
+         //System.out.println(totalPrice);
       
+      }
       
    }
 
