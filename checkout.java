557a1c638c5bc7be00a2aa023b5e71e0c7428c83
BlastedGlass07 <69607761+BlastedGlass07@users.noreply.github.com>
2021-01-16 14:16:10 -0500

    The "No" answer when it says "Do you want to shop" now works
    1/16/2021 - Jayden Kim

--- checkout.java
+++ checkout.java
@@ -15,90 +15,89 @@ class checkout
       shopping = JOptionPane.showInputDialog("Are you sure you want to shop?");
       
       //if statement
-      if (shopping.equals("yes"))
+      if (shopping.equals("yes") || shopping.equals ("Yes"))
       {
          JOptionPane.showMessageDialog(null ,"Thank you for choosing Walmart");
-      }
+         
+         String Systeminput;
+         //input list
+         JOptionPane.showMessageDialog(null ,"type done whenever your done");
+         ArrayList<String> PriceList = new ArrayList<String>( );
+         ArrayList<String> shoppingList = new ArrayList<String>( );
+         float x, price;
+         
+         do
+         {
+         
+            //user input
+            Systeminput = JOptionPane.showInputDialog(null, "type the name of the item");
+         
+            //ifs
+            if(Systeminput.equals("done"))
+            {
+            x = 2;
+            }
+            else
+            {
+               shoppingList.add(Systeminput);
+               System.out.println(shoppingList);
+               x = 1;
+               Systeminput = JOptionPane.showInputDialog(null, "type the price of the item");
+            
+               if(Systeminput.equals("done") || Systeminput.equals("Done"))
+               {
+                  x = 2;
+               }
+            
+               else
+               {
+                  PriceList.add(Systeminput);
+                  System.out.println(PriceList);  
+               }
+            }
+        
+         }
+        
+        
+         //Output
+         while(x < 2);
+         
+         String tempstring = "Items List\n";
+         for(String shoplist : shoppingList)
+         {
+            tempstring += shoplist + " Price: " + PriceList + "\n";
+         }
+        
+         float sum = 0;
+         for(int i=0; i < PriceList.size(); i++)
+         {
+            sum = sum + Float.parseFloat(PriceList.get(i));
+         }
+         
+         //calculate and show taxes
+         String tempstring1 = "";
+         if(sum > 0)
+         {
+            double totalPrice = sum * 0.06 + sum;  //totalPrice is total price with tax of 6%
+            //System.out.println(totalPrice);
+            tempstring1 = Double.toString(totalPrice);
+         }
+         
+         JOptionPane.showMessageDialog(null, tempstring + "\n$" + sum + " is your total price" + 
+         "\n" + tempstring1 + " is your total price including tax)");
       
-      else if (shopping.equals("no"))
+      }   
+      
+         
+      if (shopping.equals("no") || shopping.equals("No"))
       {
          JOptionPane.showMessageDialog(null ,"Goodbye");
       }
       
       else
       {
-         JOptionPane.showMessageDialog(null ,"Please answer with yes or no");      
+         JOptionPane.showMessageDialog(null ,"Please answer with a \"yes\" or \"no\".");      
       }   
-
-      
-      
-      
-      String Systeminput;
-      //input list
-      JOptionPane.showMessageDialog(null ,"type done whenever your done");
-      ArrayList<String> PriceList = new ArrayList<String>( );
-      ArrayList<String> shoppingList = new ArrayList<String>( );
-      float x, price;
-      
-      do
-      {
-      
-         //user input
-         Systeminput = JOptionPane.showInputDialog(null, "type the name of the item");
-      
-         //ifs
-         if(Systeminput.equals("done"))
-         {
-         x = 2;
-         }
-         else
-         {
-            shoppingList.add(Systeminput);
-            System.out.println(shoppingList);
-            x = 1;
-            Systeminput = JOptionPane.showInputDialog(null, "type the price of the item");
-         
-            if(Systeminput.equals("done"))
-            {
-               x = 2;
-            }
-         
-            else
-            {
-               PriceList.add(Systeminput);
-               System.out.println(PriceList);  
-            }
-         }
-     
-      }
-     
-     
-      //Output
-      while(x < 2);
-      
-      String tempstring = "Items List\n";
-      for(String shoplist : shoppingList)
-      {
-         tempstring += shoplist + " Price: " + PriceList + "\n";
-      }
-     
-      float sum = 0;
-      for(int i=0; i < PriceList.size(); i++)
-      {
-         sum = sum + Float.parseFloat(PriceList.get(i));
-      }
-      
-      //calculate and show taxes
-      String tempstring1 = "";
-      if(sum > 0)
-      {
-         double totalPrice = sum * 0.06 + sum;  //totalPrice is total price with tax of 6%
-         //System.out.println(totalPrice);
-         tempstring1 = Double.toString(totalPrice);
-      }
-      
-      JOptionPane.showMessageDialog(null, tempstring + "\n$" + sum + " is your total price" + 
-      "\n" + tempstring1 + " is your total price including tax)");
       
    }
 
